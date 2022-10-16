f3f378a34ea3550379cea3d2bf52cdbf5326b98b
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-10-16 12:17:39 +0900

    test: api

--- pages/api/posts/[id].ts
+++ pages/api/posts/[id].ts
@@ -1,14 +1,16 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { getPost } from '../../../service/post.service';
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import { getPost } from '../../../service/post.service';
 
-export default function postsHandler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { query } = req;
-  const post = getPost(query.id as string);
+// export default function postsHandler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const { query } = req;
+//   const post = getPost(query.id as string);
 
-  return post
-    ? res.status(200).json(post)
-    : res.status(404).json({ message: `Posts with id: ${ query.id } not found.` });
-}
+//   return post
+//     ? res.status(200).json(post)
+//     : res.status(404).json({ message: `Posts with id: ${ query.id } not found.` });
+// }
+
+export default {}
--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -1,13 +1,14 @@
-import { NextApiResponse, NextApiRequest } from 'next';
-import { getPostNames } from '../../../service/post.service';
+// import { NextApiResponse, NextApiRequest } from 'next';
+// import { getPostNames } from '../../../service/post.service';
 
-export default function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const postNames = getPostNames();
+// export default function handler(
+//   _req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const postNames = getPostNames();
   
-  return postNames
-    ? res.status(200).json(postNames)
-    : res.status(404).json({ message: `Posts not found.` });
-}
+//   return postNames
+//     ? res.status(200).json(postNames)
+//     : res.status(404).json({ message: `Posts not found.` });
+// }
+export default {}
