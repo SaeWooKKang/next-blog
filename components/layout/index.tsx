8eda4750517185d073f783d09b3d98ef8a4e2612
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-10-18 21:44:00 +0900

    design: footer

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 import style from './index.module.css';
+import Image from 'next/image'
+
+import email from '../../public/assets/icons/email.png';
+import github from '../../public/assets/icons/github.png';
 
 type Props = {
   children: React.ReactNode
@@ -16,8 +20,23 @@ const Layout = ({ children }: Props) => {
       </main>
     
       <footer className={ style.footer } >
-        <div>git 주소</div>
-        <div>이메일 주소</div>
+        <div>
+          <a href='https://github.com/SaeWooKKang' className={style.image} >
+            <Image 
+              alt="github"
+              width={22}
+              height={22}
+              src={ github } />
+          </a>
+          <a href="mailto:pac11.dev@gmail.com">
+            <Image 
+              alt="email"
+              width={22}
+              height={22}
+              src={ email } />
+          </a>
+        </div>
+        <div>saewookkang • ©2022</div>
       </footer>
     </div>
   );
