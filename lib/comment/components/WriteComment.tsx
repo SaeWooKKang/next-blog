bef4e17ac066878131171225069684e2fce44d0a
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-10-26 11:04:49 +0900

    fix: timer

--- lib/comment/components/WriteComment.tsx
+++ lib/comment/components/WriteComment.tsx
@@ -8,7 +8,7 @@ import {
 
 import git from '../comment.service';
 import style from '../style/writeComment.module.css';
-import Loading from '../../../components/loading'
+import { Loading } from '../index';
 
 interface Props {
   handleComment: Dispatch<SetStateAction<never[]>>
@@ -26,31 +26,49 @@ const WriteComment = (props: Props) => {
     const id = idRef.current!.value;
     const comment = commentRef.current!.value;
     const title = git.getPostTitle();
+    
+    if (!id || !comment) {
+      alert('텍스트를 입력해주세요.');
+      return;
+    }
 
     const config = {
       id, 
       comment,
       title
     };
-
-    git.createComment(config)
-      .then(_ => {
+ 
+    git.createComment(config) 
+      .then(_ => { 
         idRef.current!.value = '';
         commentRef.current!.value = '';
       })
       .then(_ => {
         setIsLoading(true);
         
-        setTimeout(async () => {    
-          const response = await git.getComments(title);
-            props.handleComment(response);
+        let timerId = setTimeout(async function tick() {
+          const newComments = await git.getComments(title);
+
+          if (newComments.length === props.comments.length) {
+            timerId = setTimeout(tick, 3000);
+          }
+          else {
+            props.handleComment(newComments);
             setIsLoading(false);
-        }, 7000)
+          }
+          
+        }, 3000);
+
+        setTimeout(() => {
+          clearTimeout(timerId);
+          setIsLoading(false);
+        }, 9000);
       });
   }
   return (
     <> 
-      {isLoading && <Loading />}
+      { isLoading && <Loading /> }
+
       <form onSubmit={onSubmitComment}>
         <input 
           className={style.name}
@@ -66,7 +84,7 @@ const WriteComment = (props: Props) => {
           <button className={style.button}>등록</button>
         </div>
       </form>
-  </>)
+  </>);
 }
 
 export default WriteComment;
