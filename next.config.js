0b0870fa24ebc1c66ac48765952bd433686be78d
jaeyoung <pjy9665@kakao.com>
2024-06-16 19:06:44 +0900

    feat: 셀프 호스팅을 위해 static export로 변경
    - 서버의 기능을 사용하여 정적 배포시 사용할 수 없는 이미지 최적화 비활성화 추가
    - ref: https://nextjs.org/docs/pages/building-your-application/deploying\#static-html-export

--- next.config.js
+++ next.config.js
@@ -4,10 +4,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
   },
 
 }
