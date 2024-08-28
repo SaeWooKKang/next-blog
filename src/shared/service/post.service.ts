SaeWooKKang/next-blog

commit 4140d68f6a8744b68d6322d3f0f6b532f04c26ae

refactor: URL 인코딩된 공백(%20) 대신 하이픈(-)을 사용하여 가독성 개선

src/shared/service/post.service.ts
@@ -15,7 +15,7 @@ const metaDataSchema = z.object({
   keyword: z.string(),
 });
 
-export type PostMeta = z.infer<typeof metaDataSchema>;
+export type PostMeta = z.infer<typeof metaDataSchema> & { url: string };
 
 export class PostService {
   private static async markdownToHTML(markdown: string) {
@@ -51,12 +51,18 @@ export class PostService {
     return new Date(b.date).getTime() - new Date(a.date).getTime();
   }
 
-  public static getAllPostNames() {
+  private static getAllPostNames() {
     return File.getFileNamesInDirectory('__posts');
   }
 
+  public static getAllPaths() {
+    return PostService.getAllPostNames().map(((postName) => postName.replaceAll(' ', '-')));
+  }
+
   public static async getHTML(id: string) {
-    const { html: htmlLikes, meta } = PostService.parsePost(id);
+    const parsedPostId = id.replaceAll('-', ' ');
+
+    const { html: htmlLikes, meta } = PostService.parsePost(parsedPostId);
     const HTML = await PostService.markdownToHTML(htmlLikes);
 
     return { html: HTML, meta };
@@ -65,7 +71,7 @@ export class PostService {
   public static async getMetaList() {
     const postsMetaList = PostService.getAllPostNames()
       .map((fileName) => PostService.parsePost(fileName))
-      .map((post) => post.meta)
+      .map(({ meta }) => ({ ...meta, url: `${meta.title.replaceAll(' ', '-')}` }))
       .sort(PostService.sortByDescendingForFileName);
 
     return postsMetaList;

src/pages/index.tsx
@@ -32,7 +32,7 @@ function PostNames() {
           key={post.keyword}
         >
           <Link
-            href={`/posts/${post.title}`}
+            href={`/posts/${post.url}`}
           >
             <p className="date">
               {post.date}

src/pages/posts/[id].tsx
@@ -9,7 +9,7 @@ import Comment from '../../domain/blog/comment';
 import { useScrollToTitle } from '../../domain/blog/posting/useScrollToTitle';
 
 export const getStaticPaths = () => {
-  const paths = PostService.getAllPostNames().map(
+  const paths = PostService.getAllPaths().map(
     (fileName) => ({ params: { id: fileName } }),
   );
 

commit 0ef843dfac466f17cf3b37cef5fae2ee93c501b9

fix: 제목에 - 존재하는 경우 존재하여 _로 대체
- fixes: 4140d68f

src/shared/service/post.service.ts
@@ -56,11 +56,11 @@ export class PostService {
   }
 
   public static getAllPaths() {
-    return PostService.getAllPostNames().map(((postName) => postName.replaceAll(' ', '-')));
+    return PostService.getAllPostNames().map(((postName) => postName.replaceAll(' ', '_')));
   }
 
   public static async getHTML(id: string) {
-    const parsedPostId = id.replaceAll('-', ' ');
+    const parsedPostId = id.replaceAll('_', ' ');
 
     const { html: htmlLikes, meta } = PostService.parsePost(parsedPostId);
     const HTML = await PostService.markdownToHTML(htmlLikes);
@@ -71,7 +71,7 @@ export class PostService {
   public static async getMetaList() {
     const postsMetaList = PostService.getAllPostNames()
       .map((fileName) => PostService.parsePost(fileName))
-      .map(({ meta }) => ({ ...meta, url: `${meta.title.replaceAll(' ', '-')}` }))
+      .map(({ meta }) => ({ ...meta, url: `${meta.title.replaceAll(' ', '_')}` }))
       .sort(PostService.sortByDescendingForFileName);
 
     return postsMetaList;