7b838603e4d8f6e51b1e3e64d66a8b13a594610d
jaeyoung <pjy9665@kakao.com>
2024-09-03 14:42:55 +0900

    feat(접근성): lighthouse 91점 -> 100점
    - 아이콘 크기 22 -> 24
    - 전경, 후경 대비 7: 1이 되도록 변경

--- src/shared/component/layout/index.tsx
+++ src/shared/component/layout/index.tsx
@@ -12,20 +12,20 @@ function Layout({ children }: Props) {
       <header className="flex justify-between max-w-[840px] h-auto px-5 py-10 mx-auto mb-10 font-['Montserrat',_sans-serif]">
         <Link href="/">saewookkang</Link>
 
-        <div className="flex">
+        <div className="flex gap-[20px]">
           <a href="https://github.com/SaeWooKKang" className={style.image}>
             <Image
               alt="github"
-              width={22}
-              height={22}
+              width={24}
+              height={24}
               src="/assets/icons/github.png"
             />
           </a>
           <a href="mailto:lemonade2929@naver.com">
             <Image
               alt="email"
-              width={22}
-              height={22}
+              width={24}
+              height={24}
               src="/assets/icons/email.png"
             />
           </a>
